4c0adaf18a46cb6d7aa0b774bb1171188374f0a8
Manuel Ajoy <manuelajoy@gmail.com>
2018-04-19 19:28:06 -0600

    Deleted unused folder

--- src/T4AzureArmTemplateGenerator/Namespaces/Input/LocationDefinitionInput.cs
+++
@@ -1,8 +0,0 @@
-namespace T4AzureArmTemplateGenerator.Namespaces.Input
-{
-	public class LocationDefinitionInput
-	{
-		public string LocationName { get; set; }
-		public string NamespaceNameSuffix { get; set; }
-	}
-}
